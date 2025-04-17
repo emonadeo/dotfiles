a4b9e59ad44b224e9e0180e9d95d9a2dbe0ac306
Emanuel Pilz <emonadeo@gmail.com>
2025-04-17 02:45:45 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "NixOS Configuration Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # TODO: Migrate to official flake once available
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      dragonfruit = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+	  ./hosts/dragonfruit/configuration.nix
+        ];
+      };
+    };
+  };
+}
