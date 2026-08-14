babe3f26b2becf2c98790a7d788ee67d72dc088e
Emanuel Pilz <emonadeo@gmail.com>
2026-08-14 15:25:00 +0200

    ai: add `gemma-4-26B` and `qwen3.8-27B`

--- nix/nixos/ai.nix
+++ nix/nixos/ai.nix
@@ -1,4 +1,4 @@
-{ moduleWithSystem, ... }:
+{ inputs, moduleWithSystem, ... }:
 {
   flake.nixosModules.ai = moduleWithSystem (
     _perSystem@{ inputs', pkgs, ... }:
@@ -6,6 +6,7 @@
     let
       # TODO: Switch to stable channel once possible
       pkgs' = inputs'.nixpkgs-unstable.legacyPackages;
+      # PERF: These models take up a lot of disk space. Choose one in the future.
       models = [
         (pkgs'.fetchFromHuggingFace {
           name = "gemma-4-12B-it-Q8_0";
@@ -15,22 +16,50 @@
           rev = "7e0fbb8205d1f4857f4606a38a65023aaeb5f544";
           hash = "sha256-RTBGQx5MxhiNe4L3JOrbAwkkgFW1OccTMwoXmkM0Hz0=";
         })
+        (pkgs'.fetchFromHuggingFace {
+          name = "gemma-4-26B-A4B-it-Q8_0";
+          backend = "lfs";
+          repoId = "ggml-org/gemma-4-26B-A4B-it-GGUF";
+          rootDir = "gemma-4-26B-A4B-it-Q8_0.gguf";
+          rev = "bb4531cda34d1ea09d9814959ed4d5833cf2a4c8";
+          hash = "sha256-gj0fOGjCI1D8Bc8I/QNBX7xSncppPDqhijumKWUFSSk=";
+        })
+        (pkgs'.fetchFromHuggingFace {
+          name = "Qwen3.8-27B-Q8_0";
+          backend = "lfs";
+          repoId = "unsloth/Qwen3.8-27B-GGUF";
+          rootDir = "Qwen3.8-27B-Q8_0.gguf";
+          rev = "fdd03b8bbd279c1694563650e79d85a2373d9934";
+          hash = "sha256-LZPcMBYZ9mtAHcvsyTxkdFD11iu0poU62tBPGXLd+kE=";
+        })
       ];
     in
     {
       environment.systemPackages = [
-        pkgs'.pi-coding-agent
+        # Wrap `pi-coding-agent` to include `node` and `npm`.
+        # This is needed for commands like `pi install npm:...`
+        # NOTE: pi extensions can not be managed by nix without home-manager.
+        # pi's config files live in $PI_CODING_AGENT_DIR together with other stateful
+        # files that get modified at runtime. As such pointing $PI_CODING_AGENT_DIR
+        # to /nix/store is not possible.
+        (inputs.wrappers-b.lib.wrapPackage ({
+          pkgs = pkgs';
+          package = pkgs'.pi-coding-agent;
+          runtimePkgs = [ pkgs.nodejs ];
+        }))
       ];
 
       services.llama-cpp = {
         enable = true;
         package = pkgs'.llama-cpp-vulkan;
-        # TODO: Try out Kimi-K3 once supported
-        # See <https://github.com/ggml-org/llama.cpp/pull/26185>
         modelsDir = pkgs.runCommand "llama-models" { } ''
           mkdir -p $out
           ${lib.join "\n" (map (model: "ln -s ${model} $out/${model.name}.gguf") models)}
         '';
+        extraFlags = [
+          "-c"
+          "16384"
+        ];
       };
     }
   );
